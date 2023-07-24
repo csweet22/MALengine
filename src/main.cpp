b33ede4fdff017686845db0b6482287f9527fdd4
colesw <colesw@bigbluebubble.com>
2023-07-24 08:40:46 -0400

    editing todo comments

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,9 @@
 	// TODO: abstract shaders somehow so that you can create instances of them and edit uniforms.
 	// TODO FUTURE: compute shader
 // TODO: imgui abstract it (create DockSpace imgui docking branch)
-// TODO: make imgui scene hierarchy show parents and children
-// TODO: enabled is broken for children
-// TODO: able to select objects
+// TODO: able to select objects, create a gameobject inspector
 // TODO: add a gameobject destroy() method, cleans up any parents and child pointers, frees data, removes from scene list.
 // TODO: try to setup raymarching SDFs
-// TODO: create a gameobject inspector
 
 int main( int argc, char* argv[])
 {
